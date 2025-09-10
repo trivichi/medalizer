afc70b819030241a502be0a6b49a6b94ec4210ba
trivichi <chirayutrivediaaaaa@gmail.com>
2025-09-10 22:04:16 +0530

    .

--- medalizer_be/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import SignupView
-
-urlpatterns = [
-    path("signup/", SignupView.as_view(), name="signup"),
-    path("login/", TokenObtainPairView.as_view(), name="login"),
-    path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-]
